98c3d922b43b98572823c3ef6ed112e85f74c215
george fourkas <giorgosfourkas.98@gmail.com>
2021-09-03 19:03:49 +0300

    Added front end switches submit button and a little text field

--- app/src/main/java/com/example/ergasiaseptemvrioy/MainActivity.java
+++ app/src/main/java/com/example/ergasiaseptemvrioy/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.ergasiaseptemvrioy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button submit = (Button) findViewById(R.id.submitButton);
+        Switch facebookSwitch = findViewById(R.id.facebookSwitch);
+        EditText editText = (EditText)findViewById(R.id.postBody);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            String postBody = editText.getText().toString();
+            boolean isOn = facebookSwitch.isChecked();
+            if (isOn){
+                Log.d("STATE", "sharing to facebook");
+            }
+
+
+
+
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/ergasiaseptemvrioy/UserInput.java
@@ -0,0 +1,21 @@
+package com.example.ergasiaseptemvrioy;
+
+public class UserInput {
+
+    public UserInput() {
+
+    }
+
+    public UserInput something() {
+        return this;
+    }
+
+    public UserInput somethingElse() {
+
+        int a = 5;
+        return this;
+    }
+
+
+
+}
